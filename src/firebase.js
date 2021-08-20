ae43b4132003e1fb69797faa01420330a24d2d0e
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-20 22:02:20 +0530

    Updated

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+
+    apiKey: "AIzaSyDvJXRDuxc-wVApcrdnyRGWWmAB0yFEgo0",
+    authDomain: "react-firebase-chatapp-cb3c1.firebaseapp.com",
+    projectId: "react-firebase-chatapp-cb3c1",
+    storageBucket: "react-firebase-chatapp-cb3c1.appspot.com",
+    messagingSenderId: "1017709138000",
+    appId: "1:1017709138000:web:81d7f0246f4900aa344155",
+    measurementId: "G-272KECHE62"
+})
+
+const db = firebaseApp.firestore{}
+
+const auth = firebase.auth{}
+
+export {db, auth, firebaseApp}
+
