63193bc8d2be75dc2222cd7e3a15ef34b431c8a1
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-21 21:58:04 +0530

    Updated

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,10 +1,19 @@
 import React from 'react'
-
+import {Button} from '@material-ui/core'
+import firebase from 'firebase'
 
 function SignIn() {
+
+    function SignInWithGoogle() {
+
+    }
+
+
     return (
         <div>
-            
+            <Butto Onclick = (signInWithGoogle)>
+                Sign In With Google
+            </Butto>
         </div>
     )
 }
