59d67624572d9ab9169a58b9d4a7d95b1530aa92
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-21 22:14:14 +0530

    Updated

--- src/firebase.js
+++ src/firebase.js
@@ -11,9 +11,8 @@ const firebaseApp = firebase.initializeApp({
     measurementId: "G-272KECHE62"
 })
 
-const db = firebaseApp.firestore {}
-
-const auth = firebase.auth {}
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
 
 export {db, auth}
 
