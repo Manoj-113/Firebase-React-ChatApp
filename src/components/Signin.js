f0824d426a5068d08d1f72daedc0772cf9aa6769
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-21 22:11:06 +0530

    Updated

--- src/components/Signin.js
+++ src/components/Signin.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Button} from '@material-ui/core'
 import firebase from 'firebase'
-import auth from '../firebase'
+import {auth} from '../firebase'
 
 function SignIn() {
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import firebase from "firebase";
+import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
 
@@ -11,9 +11,9 @@ const firebaseApp = firebase.initializeApp({
     measurementId: "G-272KECHE62"
 })
 
-const db = firebaseApp.firestore{}
+const db = firebaseApp.firestore {}
 
-const auth = firebase.auth{}
+const auth = firebase.auth {}
 
-export {db, auth, firebaseApp}
+export {db, auth}
 
