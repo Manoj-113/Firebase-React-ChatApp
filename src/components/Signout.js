c9f654b5af0bbecffdb559dddac446e48c5b3b16
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-22 22:56:39 +0530

    Updated

---
+++ src/components/Signout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {auth} from '../firebase'
+import {Button} from '@material-ui/core'
+
+
+function Signout() {
+    return (
+        <div>
+            <Button onClick={() = >auth.Signout()}>
+        </div>
+    )
+}
+
+export default Signout
