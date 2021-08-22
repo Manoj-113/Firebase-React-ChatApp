3b5b280f32e60ca5da8a42c7c834d8d2fd7c5467
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-22 22:46:20 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@
 import './App.css';
 import Chat from './components/Chat';
 import SignIn from './components/Signin';
-
+import {useAuthState} from 'react-firebase-hooks';
+import {auth} from './firebase';
 
 function App() {
+
+  const {user} = useAuthState(auth)
+
   return (
     <>
       <SignIn/>
