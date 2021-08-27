07ad43b022eeb14007b6ce84235a5d388a27d746
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-27 22:52:39 +0530

    updated

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, {userState, useEffect} from 'react'
 import SignOut from './SignOut'
 
 function Chat() {
+
+    const [messages,setMessages] = userState([])
+
     return (
         <div>
 
             <SignOut></SignOut>
 
             Chat
-            
+
+        
         </div>
     )
 }
