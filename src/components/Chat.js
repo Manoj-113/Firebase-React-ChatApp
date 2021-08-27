c82b5e0b9335cbc5f51d64e73fbe5ba49f96ef61
Manoj-113 <66235470+Manoj-113@users.noreply.github.com>
2021-08-27 23:12:42 +0530

    Updated

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,13 +4,22 @@ import SignOut from './SignOut'
 function Chat() {
 
     const [messages,setMessages] = userState([])
-
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
     return (
         <div>
 
             <SignOut></SignOut>
 
             Chat
+            {messages.map(({id,text,photoURL}) => {
+                <div>
+
+                </div>
+            })}
 
         
         </div>
